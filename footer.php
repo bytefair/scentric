3e845d455e9817462dae0770ebcabebfe46592a7
Paul Graham <homosaur@gmail.com>
2012-03-27 16:11:38 -0500

    moved the javascript to the functions file like WP wants

--- footer.php
+++ footer.php
@@ -21,18 +21,6 @@
 	</footer><!-- .site-footer .site-footer -->
 </div><!-- #page .hfeed .site -->
 
-
-<!-- JavaScript at the bottom for fast page loading -->
-
-<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
-
-<!-- scripts concatenated and minified via build script -->
-<script src="<?php bloginfo('template_directory'); ?>/js/plugins.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
-<!-- end scripts -->
-
 <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
    mathiasbynens.be/notes/async-analytics-snippet -->
 <script>
--- functions.php
+++ functions.php
@@ -104,16 +104,21 @@ function scentric_scripts() {
 
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
+	/* loads local copy of Modernizr */
+	wp_enqueue_script( 'modernizr', get_template_directory_uri().'/js/libs/modernizr-2.5.3.min.js');
+
 	wp_enqueue_script( 'jquery' );
 
-	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', 'jquery', '20120206', true );
+	/* jQuery plugins and scripts load */
+	wp_enqueue_script( 'jquery-plugins', get_template_directory_uri().'/js/plugins.js', array('jquery'));
+	wp_enqueue_script( 'jquery-script', get_template_directory_uri().'/js/script.js', array('jquery'));
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image( $post->ID ) ) {
-		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/libs/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'scentric_scripts' );
