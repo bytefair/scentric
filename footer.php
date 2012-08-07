66d97225e4fc462580ba94152886cf19e4e7498d
Paul Graham <pgraham@cordelllaw.com>
2012-08-07 15:34:30 -0500

    switched invalid rel attributes to nofollow, because they probably should be

--- footer.php
+++ footer.php
@@ -12,9 +12,9 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'scentric_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'scentric' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'scentric' ), 'WordPress' ); ?></a>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'scentric' ); ?>" rel="nofollow"><?php printf( __( 'Proudly powered by %s', 'scentric' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'scentric' ), '<a href="http://github.com/bytefair/scentric">Scentric</a>', '<a href="http://bytefair.com/" rel="designer">Paul Edmon Graham</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'scentric' ), '<a href="http://github.com/bytefair/scentric">Scentric</a>', '<a href="http://bytefair.com/" rel="nofollow">Paul Edmon Graham</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer .site-footer -->
 </div><!-- #page .hfeed .site -->
