6661b57531663e3d166c45ad003798fd6cb1f3d9
Paul Graham <paul@bytefair.com>
2012-09-24 20:34:48 -0500

    added Adobe Edge Web Fonts script

--- functions.php
+++ functions.php
@@ -181,6 +181,7 @@ function scentric_scripts() {
 	wp_enqueue_script( 'jquery-plugins', get_template_directory_uri().'/js/plugins.js', array('jquery'), '0',true);
 	wp_enqueue_script( 'jquery-script', get_template_directory_uri().'/js/script.js', array('jquery'), '0', true);
 	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/vendor/small-menu.js', array( 'jquery' ), '20120206', true );
+  wp_enqueue_script( 'edge-web-fonts', 'http://use.edgefonts.net/league-gothic.js');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
